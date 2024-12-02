d000d5a8620e6ee47abdd7f04634461eaba5126f
Abhishek kumar <96790708+abhishek9640@users.noreply.github.com>
2024-12-03 00:06:14 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -88,15 +88,6 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1NjvgxERhYXstmeg8R-YVHLmGTdkrlBOc/view?usp=sharing"
-          download
-        >
-          Download CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
-
-        <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://linkedin.com/in/abhishek-54-kr/"
           target="_blank"
